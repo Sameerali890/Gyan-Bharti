3a75e814cf696eed7517f85d0146199dbe052138
Sameerali890 <141541073+Sameerali890@users.noreply.github.com>
2025-01-28 13:42:34 +0530

    Modal Add

--- js/main.js
+++ js/main.js
@@ -107,3 +107,10 @@
     
 })(jQuery);
 
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
